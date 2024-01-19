393c1daad8c8d6fcc9e07cddb02208ab4aafbedd
mingmoth <mingmoth@gmail.com>
2024-01-19 22:30:53 +0800

    project init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <div class="wrapper">
+      <h1>This is an about page</h1>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
