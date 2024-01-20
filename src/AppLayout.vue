a05dfe8f4acdeadfafd3bce4de1b5ce7bd68353b
mingmoth <mingmoth@gmail.com>
2024-01-20 20:28:04 +0800

    fix: add AppLayout.vue for new vue app entry

---
+++ src/AppLayout.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import Menubar from 'primevue/menubar'
+import { ref } from 'vue';
+
+const menuItems = ref([
+    {
+        label: 'Home',
+        icon: 'pi pi-fw pi-home',
+        to: '/'
+    },
+    {
+        label: 'Lang',
+        icon: 'pi pi-fw pi-info-circle',
+        items: [
+            {
+                label: 'en',
+            },
+            {
+                label: 'zh-tw',
+            }
+        ]
+    }
+])
+</script>
+
+<template>
+    <Menubar :model="menuItems">
+    </Menubar>
+    <RouterView />
+</template>
